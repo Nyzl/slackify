0d4cf58a52b6e321851105f4cb6eea4a20c56544
Ian <ian.ansell.uk@gmail.com>
2019-03-25 17:39:33 +0000

    i don't know what we did, but it works

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask,render_template,request,redirect
-import requests, os
+import requests, os, json
 import bowie
+from boto.s3.connection import S3Connection
+
 
 app = Flask(__name__)
 
@@ -12,19 +14,15 @@ def deftones():
 
 @app.route('/slack',methods=['POST'])
 def weezer():
+    BOT_USER_TOKEN = os.environ['BOT_USER_TOKEN']
     payload = request.get_json()
     response = bowie.ziggy(payload)
-    return sendToSlack(response)
-    #return str(payload["challenge"])
 
+    payload = {"text":"goose", "channel":"CH02K9AEA"}
+    headers = {"Content-type":"application/json;charset=utf-8", "Authorization":"Bearer "+ str(BOT_USER_TOKEN)}
+    r = requests.post("https://slack.com/api/chat.postMessage", headers=headers, data=json.dumps(payload))
 
-def sendToSlack(response):
-    BOT_USER_TOKEN = os.environ['BOT_USER_TOKEN']
-    payload = {'text': response, 'channel': 'CH02K9AEA'}
-    headers = {'Content-type': 'application/json', 'Authorization':'Bearer '+BOT_USER_TOKEN}
-    r = requests.post("https://slack.com/api/chat.postMessage", headers=headers, data=payload)
-
-
+    return str(r.text)
 
 
 if __name__ == "__main__":
