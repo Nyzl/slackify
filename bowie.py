2d95ad2e9cd034b0416e031ed932dda3a9a7b16e
Adam Smith <smifaye@gmail.com>
2019-03-26 11:58:31 +0000

    Update bowie.py

--- bowie.py
+++ bowie.py
@@ -8,7 +8,9 @@ def ziggy (payload):
   if payload["type"] == "app_mention":
     return "i'm in the curve, man"
   else:
-    return "I'm confused"
+    #return "I'm confused"
+    return str(payload["type"])
+    
 
 if __name__ == "__main__":
     ziggy.run()
