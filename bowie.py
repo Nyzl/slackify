da51fbfb8adea8ea1f098054a5a09981be086b3d
Adam Smith <smifaye@gmail.com>
2019-03-26 11:09:32 +0000

    Update bowie.py

--- bowie.py
+++ bowie.py
@@ -1,6 +1,3 @@
-#built on the brilliant work by Adam J Calhourn from his post
-#https://medium.com/@neuroecology/punctuation-in-novels-8f316d542ec4#.tz6i4h1ub
-
 import app
 import string
 import json
@@ -8,7 +5,7 @@ from urllib.request import urlopen
 import re
 
 def ziggy (payload):
-    return "i'm in space, man"
+    return "i'm in the curve, man"
 
 if __name__ == "__main__":
     ziggy.run()
