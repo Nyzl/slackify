ab58ac33bf1065015b1f3747575986c84305c863
Adam Smith <smifaye@gmail.com>
2019-03-26 14:27:13 +0000

    Update bowie.py

--- bowie.py
+++ bowie.py
@@ -5,25 +5,25 @@ from urllib.request import urlopen
 import re
 
 def ziggy (payload):
-  if payload["event"]["type"] == "app_mention":
-    if "make a playlist" in payload["event"]["text"] or "create a playlist" in payload["event"]["text"] or "create playlist" in payload["event"]["text"] or "make playlist" in payload["event"]["text"]:
-      return {
-        "text":"So, ya wanna make a playlist, eh?",
-        "attachments":[{
-          "text": "",
-          "color": "#3AA3E3",
-          "attachment_type": "default",
-          "actions": [{
-            "name": "create_playlist",
-            "text": ":guitar: Let's go!",
-            "type": "button",
-            "value": "create_playlist"
-            }]
-          }]
-        }
+  	if payload["event"]["type"] == "app_mention":
+    	if "make a playlist" in payload["event"]["text"] or "create a playlist" in payload["event"]["text"] or "create playlist" in payload["event"]["text"] or "make playlist" in payload["event"]["text"]:
+      		return {
+        		"text":"So, ya wanna make a playlist, eh?",
+        		"attachments":[{
+					"text": "",
+					"color": "#3AA3E3",
+					"attachment_type": "default",
+          			"actions": [{
+            			"name": "create_playlist",
+            			"text": ":guitar: Let's go!",
+            			"type": "button",
+            			"value": "create_playlist"
+            		}]
+          		}]
+        	}
     
     else:
-      return "What?"
+      	return "What?"
 
 if __name__ == "__main__":
     ziggy.run()
