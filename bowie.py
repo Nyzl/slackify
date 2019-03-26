39ce45e12dc9a838dc64de227a852fd6b6c219c8
Adam Smith <smifaye@gmail.com>
2019-03-26 12:34:19 +0000

    Update bowie.py

--- bowie.py
+++ bowie.py
@@ -6,7 +6,7 @@ import re
 
 def ziggy (payload):
   if payload["event"]["type"] == "app_mention":
-    if "make a playlist" in payload["event"]["text"]:
+    if "make a playlist" or "create a playlist" in payload["event"]["text"]:
       return "So, ya wanna make a playlist, eh?"
     else:
       return "i'm in the curve, man"
