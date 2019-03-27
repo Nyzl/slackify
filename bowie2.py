8422daf4c1d4f1265727b3a34edede8cf1ecb86b
Ian <ian.ansell.uk@gmail.com>
2019-03-27 17:11:37 +0000

    gosh

--- bowie2.py
+++ bowie2.py
@@ -55,6 +55,6 @@ def ziggy(payload, request):
             response["attachments"] = ""
             return response
     except:
-        response["text"] =  str(payload["payload"])
+        response["text"] =  str(request)
         response["attachments"] = ""
         return response
