de02652e4e89ac0b9adc79baa86a4e18c9ec32bb
Ian Ansell <ian.ansell.uk@gmail.com>
2019-03-27 21:25:44 +0000

    order

--- bowie3.py
+++ bowie3.py
@@ -20,8 +20,7 @@ else:
 
 def ziggy(payload):
     response = {}
-    if payload["type"] == "event_callback" and payload["event"]["type"] == "app_mention" and bool(re.search('(make|create).*playlist', message)):
-        message = payload["event"]["text"]
+    if payload["type"] == "event_callback" and payload["event"]["type"] == "app_mention" and bool(re.search('(make|create).*playlist', payload["event"]["text"])):
         response["text"] = "So, ya wanna make a playlist, eh?"
         response["attachments"] = ""
         return response
