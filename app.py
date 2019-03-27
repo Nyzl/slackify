e576648893fdb536e851fe3d8d268e4550652858
Ian Ansell <ian.ansell.uk@gmail.com>
2019-03-27 20:26:03 +0000

    now

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def deftones():
 def weezer():
     BOT_USER_TOKEN = os.environ['BOT_USER_TOKEN']
     in_payload = request.get_json()
-    response = bowie3.ziggy(in_payload, request.data)
+    response = bowie3.ziggy(in_payload)
 
     out_payload = {
     "channel": "CH02K9AEA",
