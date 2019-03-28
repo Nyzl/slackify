2162ac43973048adf142af3b5048a5b0b8e7d95c
Ian <ian.ansell.uk@gmail.com>
2019-03-28 11:11:10 +0000

    hmmmmm

--- app.py
+++ app.py
@@ -107,9 +107,9 @@ def callback():
     #playlist_api_endpoint = "https://api.spotify.com/v1/users/xu15ggjeufiorfq5pozsze64z/playlists"
     playlists_response = requests.post(playlist_api_endpoint, headers=authorization_header, data=json.dumps(data))
     playlist_data = playlists_response.json()
-    playlist_url = playlist_data["external_links"]["spotify"]
+    #playlist_url = playlist_data["external_links"]["spotify"]
 
-    response["text"] = "There we are, I've made a playlist called " + playlist_name + ". Here is the link: " + playlist_url
+    response["text"] = "There we are, I've made a playlist called " + playlist_name + ". Here is the link: " + str(playlist_data)
     return slack_post(response)
 
 def slack_post(response):
