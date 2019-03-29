5b2cbffadebaafa0a0448aab6fbcca9102b14383
Adam Smith <smifaye@gmail.com>
2019-03-29 08:57:11 +0000

    Update bowie3.py

--- bowie3.py
+++ bowie3.py
@@ -45,8 +45,8 @@ def ziggy(payload, url):
                 }
             ]
             return response
-        elif bool(re.search('(?:hello|hi|hey)', payload["event"]["text"])):
-            response["text"] = time + "<@" + payload["user"]["name"] + ">! What's up?",
+        elif bool(re.search('(?:hello|hi|hey|morning)', payload["event"]["text"])):
+            response["text"] = time + "! What's up?",
             response["attachments"] = ""
 
             return response
